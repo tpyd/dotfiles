97bf2458b0df05d03a43301c670f004d21c6d6b4
tpyd <112725276+tpyd@users.noreply.github.com>
2025-04-05 14:09:41 +0200

    Added textobjects

---
+++ nvim/init.lua
@@ -0,0 +1,132 @@
+vim.env.LANG = "en_US"
+
+vim.g.mapleader = " "
+
+vim.opt.clipboard = "unnamedplus"
+vim.opt.expandtab = true
+vim.opt.ignorecase = true
+vim.opt.inccommand = "split"
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.scrolloff = 8
+vim.opt.shiftwidth = 4
+vim.opt.splitbelow = true
+vim.opt.splitright = true
+vim.opt.tabstop = 4
+vim.opt.termguicolors = true
+vim.opt.virtualedit = "block"
+
+-- Bootstrap lazy.nvim
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+
+if not vim.loop.fs_stat(lazypath) then
+    local out = vim.fn.system({
+        "git",
+        "clone",
+        "--filter=blob:none",
+        "--branch=stable",
+        "https://github.com/folke/lazy.nvim.git",
+        lazypath
+    })
+
+    if vim.v.shell_error ~= 0 then
+        vim.api.nvim_echo({
+            { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+            { out, "WarningMsg" },
+            { "\nPress any key to exit..." },
+        }, true, {})
+
+        vim.fn.getchar()
+        os.exit(1)
+    end
+end
+
+vim.opt.rtp:prepend(lazypath)
+
+require("lazy").setup({
+    {
+        "ellisonleao/gruvbox.nvim",
+        config = function()
+            require("gruvbox").setup({
+                transparent_mode = true
+            })
+            vim.cmd.colorscheme("gruvbox")
+        end
+    },
+    {
+        "nvim-treesitter/nvim-treesitter",
+        config = function()
+            require("nvim-treesitter.configs").setup({
+                ensure_installed = {
+                    "query",
+                    "lua",
+                    "rust",
+                    "python",
+                    "c_sharp",
+                    "toml",
+                    "markdown",
+                    "xml"
+                },
+                auto_install = true,
+                highlight = {
+                    enable = true
+                },
+                incremental_selection = {
+                    enable = true,
+                    keymaps = {
+                        init_selection = "<Leader>ss",
+                        node_incremental = "<Leader>si",
+                        scope_incremental = "<Leader>sc",
+                        node_decremental = "<Leader>sd"
+                    }
+                },
+                textobjects = {
+                    select = {
+                        enable = true,
+                        lookahead = true,
+                        keymaps = {
+                            ["af"] = "@function.outer",
+                            ["if"] = "@function.inner",
+                            ["ac"] = "@class.outer",
+                            ["ic"] = "@class.inner",
+                            -- ["as"] = "@block.outer",
+                            -- ["is"] = "@block.inner"
+                            -- ["as"] = { query = "@local.scope", query_group = "locals", desc = "Select language scope" },
+                        },
+                        selection_modes = {
+                            ['@parameter.outer'] = 'v',
+                            ['@function.outer'] = 'v',
+                            ['@class.outer'] = 'v',
+                            ['@block.outer'] = 'v'
+                        },
+                        include_surrounding_whitespace = false
+                    }
+                }
+            })
+        end
+    },
+    {
+        "nvim-treesitter/nvim-treesitter-textobjects",
+    }
+})
+
+textobjects = {
+    select = {
+        enable = true,
+        lookahead = true,
+        keymaps = {
+            ["af"] = "@function.outer",
+            ["if"] = "@function.inner",
+            ["ac"] = "@class.outer",
+            ["ic"] = "@class.inner",
+            ["as"] = { query = "@local.scope", query_group = "locals", desc = "Select language scope" },
+        },
+        selection_modes = {
+            ['@parameter.outer'] = 'v',
+            ['@function.outer'] = 'v',
+            ['@class.outer'] = 'v',
+            ['@block.outer'] = 'v'
+        },
+        include_surrounding_whitespace = false
+    }
+}
