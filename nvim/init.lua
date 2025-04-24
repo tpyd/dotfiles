88853f1567ead9feb2685565b72e1c5491b5fe51
tpyd <112725276+tpyd@users.noreply.github.com>
2025-04-24 21:49:17 +0200

    Fixed harpoon config

--- nvim/init.lua
+++ nvim/init.lua
@@ -158,15 +158,13 @@ require("lazy").setup({
     {
         "ThePrimeagen/harpoon",
         config = function ()
-            require("harpoon").setup({})
-            local mark = require("harpoon.mark")
-            local ui = require("harpoon.ui")
-            vim.keymap.set("n", "<leader>a", mark.add_file)
-            vim.keymap.set("n", "<C-e>", ui.toggle_quick_menu)
-            vim.keymap.set("n", "<C-h>", function() ui.nav_file(1) end)
-            vim.keymap.set("n", "<C-t>", function() ui.nav_file(2) end)
-            vim.keymap.set("n", "<C-n>", function() ui.nav_file(3) end)
-            vim.keymap.set("n", "<C-s>", function() ui.nav_file(4) end)
+            local harpoon = require("harpoon").setup({})
+            vim.keymap.set("n", "<leader>a", function() harpoon:list():add() end)
+            vim.keymap.set("n", "<C-e>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
+            vim.keymap.set("n", "<C-h>", function() harpoon:list():select(1) end)
+            vim.keymap.set("n", "<C-t>", function() harpoon:list():select(2) end)
+            vim.keymap.set("n", "<C-n>", function() harpoon:list():select(3) end)
+            vim.keymap.set("n", "<C-s>", function() harpoon:list():select(4) end)
         end
     }
 })
