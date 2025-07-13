d17f84dd069d70ecd063da4f17eac5ee7ae06398
tpyd <112725276+tpyd@users.noreply.github.com>
2025-07-13 23:09:40 +0200

    Missing opts

--- nvim/init.lua
+++ nvim/init.lua
@@ -175,7 +175,7 @@ require("lazy").setup({
 local on_attach = function(_, bufnr)
     local opts = { noremap = true, buffer = bufnr }
 
-    vim.keymap.set('n', 'gd', vim.lsp.buf.definition)
+    vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
     vim.keymap.set('n', 'gD', vim.lsp.buf.declaration)
     vim.keymap.set('n', 'gi', vim.lsp.buf.implementation)
     vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
