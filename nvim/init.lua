2a7691765aca99698c618a3a17b5dea0662fe014
tpyd <112725276+tpyd@users.noreply.github.com>
2025-07-13 21:57:27 +0200

    Added better lsp keymaps

--- nvim/init.lua
+++ nvim/init.lua
@@ -172,6 +172,26 @@ require("lazy").setup({
     }
 })
 
+local on_attach = function()
+    local opts = { noremap = true }
+    vim.keymap.set('n', 'gd', vim.lsp.buf.definition)
+    vim.keymap.set('n', 'gD', vim.lsp.buf.declaration)
+    vim.keymap.set('n', 'gi', vim.lsp.buf.implementation)
+    vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
+    vim.keymap.set('n', 'K', vim.lsp.buf.hover)
+    vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help)
+    vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename)
+    vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action)
+    vim.keymap.set('n', '[d', vim.diagnostic.jump, { count = 1, float = true})
+    vim.keymap.set('n', ']d', vim.diagnostic.jump, { count = -1, float = true})
+    vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float)
+    vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist)
+end
+
+vim.lsp.config("*", {
+    on_attach = on_attach
+})
+
 vim.lsp.config("lua_ls", {
     on_init = function(client)
         if client.workspace_folders then
