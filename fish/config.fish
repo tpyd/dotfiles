78bce050cbd7c2cd774eab8c3706fd693acacbf0
tpyd <112725276+tpyd@users.noreply.github.com>
2025-08-13 18:27:09 +0200

    Moved environment variables to fish universal variables

--- fish/config.fish
+++ fish/config.fish
@@ -1,7 +1,2 @@
-set -gx EDITOR /opt/nvim-linux-x86_64/bin/nvim
 set fish_greeting
-
-# Explicitly add paths to avoid tmux $PATH shenanigans
-fish_add_path --path $HOME/.cargo/bin
-fish_add_path --path $HOME/.local/bin
 
